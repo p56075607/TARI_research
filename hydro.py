be199ed48aa70d38e77c9a685cd472aca988841b
p56075607 <p56075607@gmail.com>
2024-03-15 09:17:03 +0800

    Refactor hydro.py to improve plot_hydro_data function

--- hydro.py
+++ hydro.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from os.path import join
 from datetime import timedelta
+import matplotlib.dates as mdates
 
 # Function to correct "24:00:00" timestamps
 def correct_timestamp(ts):
@@ -32,42 +33,44 @@ def read_hydro_data(data_path):
     hourly_avg.drop('BattV', axis=1, inplace=True)
 
     return hourly_avg, daily_rainfall
+
+def plot_hydro_data(hourly_avg, daily_rainfall, plot_target):
+    fig, ax1 = plt.subplots(figsize=(20, 8))
+    for column in hourly_avg.columns:
+        if column in plot_target:
+            print(column)
+            # ax1.scatter(hourly_avg.index, hourly_avg[column], s=1, marker='o', label=column)
+            ax1.plot(hourly_avg.index, hourly_avg[column],linewidth=1, label=column)
+            ax1.set_title('Hourly Averages of Hydrological Data and Rainfall')
+            ax1.set_xlabel('Time')
+            ax1.set_ylabel('Average Value')
+            ax1.set_ylim(0, 100)        
+    ax1.xaxis.set_major_locator(mdates.WeekdayLocator(byweekday=mdates.MONDAY))
+    ax1.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+    ax1.grid(linestyle='--',linewidth=0.5)
+    # Rotate dates for better readability
+    plt.xticks(rotation=45)
+    plt.tight_layout()  # Adjust layout to make room for the rotated date labels
+
+    if 'Rain_mm_Tot' in hourly_avg.columns:
+        ax2 = ax1.twinx()  # Create a second Y-axis sharing the same X-axis
+        ax2.bar(daily_rainfall.index, daily_rainfall, width=1, alpha=0.3, color='c', label='Rainfall')
+        ax2.set_ylabel('Rainfall (mm)', color='c')  # Set label for the secondary Y-axis
+        ax2.tick_params(axis='y', labelcolor='c')  # Set ticks color for the secondary Y-axis
+
+    fig.legend(loc="upper right", bbox_to_anchor=(0.9,0.9))
+
 # %%
 # Read the CHUT hydrological data
 data_path = join("data","external","CHUT_1120818.dat")
 hourly_avg, daily_rainfall = read_hydro_data(data_path)
-plot_target = ['Result_30cm_Avg', 'Result_50cm_Avg', 'Result_100cm_Avg']
-fig, ax1 = plt.subplots(figsize=(20, 8))
-for column in hourly_avg.columns:
-    if column in plot_target:
-        print(column)
-        ax1.scatter(hourly_avg.index, hourly_avg[column], s=1, marker='o', label=column)
-        ax1.set_title('Hourly Averages of Hydrological Data and Rainfall')
-        ax1.set_xlabel('Time')
-        ax1.set_ylabel('Average Value')
-        ax1.set_ylim(0, 100)
+plot_target = ['Result_10cm_Avg', 'Result_20cm_Avg', 'Result_30cm_Avg',
+    'Result_40cm_Avg', 'Result_50cm_Avg', 'Result_60cm_Avg',
+    'Result_80cm_Avg', 'Result_100cm_Avg']
+plot_hydro_data(hourly_avg, daily_rainfall, plot_target)
 # %%
-# Plot setup
-fig, ax1 = plt.subplots(figsize=(20, 8))
-# Plotting other columns on the primary Y-axis
-for column in hourly_avg.columns:
-    if column != 'Rain_mm_Tot':
-        ax1.scatter(hourly_avg.index, hourly_avg[column], s=1, marker='o', label=column)
-
-
-if 'Rain_mm_Tot' in hourly_avg.columns:
-    ax2 = ax1.twinx()  # Create a second Y-axis sharing the same X-axis
-    ax2.bar(daily_rainfall.index, daily_rainfall, width=1, alpha=0.3, color='c', label='Rainfall')
-    ax2.set_ylabel('Rainfall (mm)', color='c')  # Set label for the secondary Y-axis
-    ax2.tick_params(axis='y', labelcolor='c')  # Set ticks color for the secondary Y-axis
-
-# Set x-axis one week interval
-ax1.xaxis.set_major_locator(plt.MaxNLocator(35))
-ax1.set_title('Hourly Averages of Hydrological Data and Rainfall')
-ax1.set_xlabel('Time')
-ax1.set_ylabel('Average Value')
-ax1.set_ylim(0, 100)  # Set y-axis limits for the primary Y-axis
-fig.legend(loc="upper left", bbox_to_anchor=(0.1,0.9))
-plt.xticks(rotation=45)
-plt.tight_layout()
-plt.show()
+plot_target = ['Result_150cm_Avg',
+       'Result_200cm_Avg', 'Result_300cm_Avg', 'Result_400cm_Avg',
+       'Result_500cm_Avg', 'Result_600cm_Avg', 'Result_700cm_Avg',
+       'Result_800cm_Avg', 'Result_900cm_Avg']
+plot_hydro_data(hourly_avg, daily_rainfall, plot_target)
