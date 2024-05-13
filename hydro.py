21041fabb49b151bf97b2be44336b203300445a7
p56075607 <p56075607@gmail.com>
2024-05-13 19:49:27 +0800

    Refactor hydro.py: Add functionality to check files in a directory and plot hydrological data

--- hydro.py
+++ hydro.py
@@ -1,4 +1,6 @@
-# %% To read and process the provided hydrological data, which is formatted as CSV
+# %% 
+# -*- coding: utf-8 -*-
+# To read and process the provided hydrological data, which is formatted as CSV
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -6,6 +8,8 @@ plt.rcParams["font.family"] = "Microsoft Sans Serif"
 from os.path import join
 from datetime import timedelta
 import matplotlib.dates as mdates
+from datetime import datetime
+import os
 
 # Function to correct "24:00:00" timestamps
 def correct_timestamp(ts):
@@ -33,9 +37,34 @@ def read_hydro_data(data_path):
     hourly_avg.drop('RECORD', axis=1, inplace=True)
     hourly_avg.drop('BattV', axis=1, inplace=True)
 
-    return hourly_avg, daily_rainfall
+    if 'Rain_mm_Tot' in hourly_avg.columns:
+        return hourly_avg, daily_rainfall
+    else:
+        return hourly_avg, None
 
-def plot_hydro_data(hourly_avg, daily_rainfall, plot_target):
+
+def check_files_in_directory(directory_path):
+    # 存儲解析出來的日期
+    dates = []
+
+    # 遍歷資料夾中的所有檔案
+    for filename in os.listdir(directory_path):
+        # 檢查檔案名稱是否符合特定格式
+        if filename.endswith('.urf'):
+            date_str = filename[:8]  # 提取日期部分
+            try:
+                # 轉換日期格式從 'YYMMDDHH' 到 datetime 對象
+                date = datetime.strptime(date_str, '%y%m%d%H')
+                dates.append(date)
+            except ValueError:
+                # 如果日期格式不正確，忽略此檔案
+                continue
+
+    return dates
+
+
+
+def plot_hydro_data(hourly_avg, daily_rainfall, plot_target,dates):
     fig, ax1 = plt.subplots(figsize=(20, 8))
     for column in hourly_avg.columns:
         if column in plot_target:
@@ -46,9 +75,11 @@ def plot_hydro_data(hourly_avg, daily_rainfall, plot_target):
             ax1.set_xlabel('Time (YYYY-MM-DD)')
             ax1.set_ylabel('Soil Moisture (%)')
             ax1.set_ylim(0, 100)        
-    ax1.xaxis.set_major_locator(mdates.WeekdayLocator(byweekday=mdates.MONDAY))
+    ax1.xaxis.set_major_locator(mdates.DayLocator(interval=1))
     ax1.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
     ax1.grid(linestyle='--',linewidth=0.5)
+    ax1.plot(dates,[3]*len(dates), 'bo')
+    ax1.set_xlim([dates[0]-timedelta(days=1),dates[-1]+timedelta(days=1)])
     # Rotate dates for better readability
     plt.xticks(rotation=45)
     plt.tight_layout()  # Adjust layout to make room for the rotated date labels
@@ -62,19 +93,46 @@ def plot_hydro_data(hourly_avg, daily_rainfall, plot_target):
     fig.legend(loc="upper right", bbox_to_anchor=(0.9,0.9))
     return fig
 
-# %%
-# Read the CHUT hydrological data
-data_path = join("data","external","CHUT_1120818.dat")
+
+# Read the hydrological data
+data_path = join("data","external","農試所水田_1120829.dat")
 hourly_avg, daily_rainfall = read_hydro_data(data_path)
+dates = check_files_in_directory(r'C:\Users\R2MSDATA\TARI_E1\urf')
 plot_target = ['Result_10cm_Avg', 'Result_20cm_Avg', 'Result_30cm_Avg',
     'Result_40cm_Avg', 'Result_50cm_Avg', 'Result_60cm_Avg',
-    'Result_80cm_Avg', 'Result_100cm_Avg']
-fig = plot_hydro_data(hourly_avg, daily_rainfall, plot_target)
-fig.savefig(join("output","hydro_data_10.png"),dpi=300, bbox_inches='tight')
-# %%
-plot_target = ['Result_150cm_Avg',
+    'Result_80cm_Avg', 'Result_100cm_Avg',
+    'Result_150cm_Avg',
        'Result_200cm_Avg', 'Result_300cm_Avg', 'Result_400cm_Avg',
        'Result_500cm_Avg', 'Result_600cm_Avg', 'Result_700cm_Avg',
        'Result_800cm_Avg', 'Result_900cm_Avg']
-fig = plot_hydro_data(hourly_avg, daily_rainfall, plot_target)
-fig.savefig(join("output","hydro_data_150.png"),dpi=300, bbox_inches='tight')
+fig = plot_hydro_data(hourly_avg, daily_rainfall, plot_target,dates)
+# fig.savefig(join("output","hydro_data_10.png"),dpi=300, bbox_inches='tight')
+
+# %%
+data_path = join("data","external","農試所竹塘站_1120818.dat")
+hourly_avg, daily_rainfall = read_hydro_data(data_path)
+dates = check_files_in_directory(r'C:\Users\R2MSDATA\TARI_E3\urf')
+plot_target = ['Result_10cm_Avg', 'Result_20cm_Avg', 'Result_30cm_Avg',
+    'Result_40cm_Avg', 'Result_50cm_Avg', 'Result_60cm_Avg',
+    'Result_80cm_Avg', 'Result_100cm_Avg',
+    'Result_150cm_Avg',
+       'Result_200cm_Avg', 'Result_300cm_Avg', 'Result_400cm_Avg',
+       'Result_500cm_Avg', 'Result_600cm_Avg', 'Result_700cm_Avg',
+       'Result_800cm_Avg', 'Result_900cm_Avg']
+fig = plot_hydro_data(hourly_avg, daily_rainfall, plot_target,dates)
+
+# %%
+data_path = join("data","external","農試所旱田_1121222.dat")
+hourly_avg, daily_rainfall = read_hydro_data(data_path)
+dates = check_files_in_directory(r'C:\Users\R2MSDATA\TARI_E2\urf')
+plot_target = ['Result_10cm_Avg', 'Result_20cm_Avg', 'Result_30cm_Avg',
+    'Result_40cm_Avg', 'Result_50cm_Avg', 'Result_60cm_Avg',
+    'Result_80cm_Avg', 'Result_100cm_Avg']
+fig = plot_hydro_data(hourly_avg, daily_rainfall, plot_target,dates)
+# %%
+# plot_target = ['Result_150cm_Avg',
+#        'Result_200cm_Avg', 'Result_300cm_Avg', 'Result_400cm_Avg',
+#        'Result_500cm_Avg', 'Result_600cm_Avg', 'Result_700cm_Avg',
+#        'Result_800cm_Avg', 'Result_900cm_Avg']
+# fig = plot_hydro_data(hourly_avg, daily_rainfall, plot_target)
+# fig.savefig(join("output","hydro_data_150.png"),dpi=300, bbox_inches='tight')
